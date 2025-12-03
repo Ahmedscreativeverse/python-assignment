a8be9e9f296abc22f010d371f0d25323639dac9b
Ahmed Olugbasa <166473506+Ahmedscreativeverse@users.noreply.github.com>
2025-12-03 12:14:33 +0100

    Add files via upload

---
+++ first_division.py
@@ -0,0 +1,17 @@
+
+
+
+first_number = int(input("Type the first number: "))
+second_number = int(input("Type the second number: "))
+
+
+
+
+    
+if second_number != 0:
+   answer = first_number / second_number
+   print(answer)
+
+else:
+   print("cannot divide by zero")
+
