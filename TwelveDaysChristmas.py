88e3bc5f447ef8fd8cc93c9d9f559cd4d26d7b3a
Ahmed Olugbasa <166473506+Ahmedscreativeverse@users.noreply.github.com>
2026-01-07 10:23:58 +0100

    Add files via upload

---
+++ TwelveDaysChristmas.py
@@ -0,0 +1,66 @@
+
+
+
+def get_day_name(day):
+    
+    if day == 1:
+        return "first"
+    elif day == 2:
+        return "second"
+    elif day == 3:
+        return "third"
+    elif day == 4:
+        return "fourth"
+    elif day == 5:
+        return "fifth"
+    elif day == 6:
+        return "sixth"
+    elif day == 7:
+        return "seventh"
+    elif day == 8:
+        return "eighth"
+    elif day == 9:
+        return "ninth"
+    elif day == 10:
+        return "tenth"
+    elif day == 11:
+        return "eleventh"
+    elif day == 12:
+        return "twelfth"
+
+
+
+def get_gift(gift_number):
+    
+    if gift_number == 12:
+        return "Twelve drummers drumming,"
+    elif gift_number == 11:
+        return "Eleven pipers piping,"
+    elif gift_number == 10:
+        return "Ten lords a-leaping,"
+    elif gift_number == 9:
+        return "Nine ladies dancing,"
+    elif gift_number == 8:
+        return "Eight maids a-milking,"
+    elif gift_number == 7:
+        return "Seven swans a-swimming,"
+    elif gift_number == 6:
+        return "Six geese a-laying,"
+    elif gift_number == 5:
+        return "Five golden rings,"
+    elif gift_number == 4:
+        return "Four calling birds,"
+    elif gift_number == 3:
+        return "Three French hens,"
+    elif gift_number == 2:
+        return "Two turtle doves and"
+    elif gift_number == 1:
+       
+        return "A partridge in a pear tree"
+
+
+
+print("🎄 THE TWELVE DAYS OF CHRISTMAS 🎄\n")
+
+
+
